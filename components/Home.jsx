5a4ec0de0e1418a8c7f7eba7ee6083e5231fd2de
Daniel Pineda Torres <dpineda.dev@gmail.com>
2025-03-28 13:09:44 -0600

    Initialized App

---
+++ components/Home.jsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+
+const Home = () => {
+    return(
+        <View>
+            <Text>
+                Holiwis desde el componente de casita
+            </Text>
+        </View>
+    )
+}
+
+export default  Home
